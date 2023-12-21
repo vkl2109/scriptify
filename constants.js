b3ed33a8e952ed6bb8eeedb080775e928e5c08a4
Vincent Le <vkl2109@columbia.edu>
2023-12-20 23:14:35 -0800

    added constants file

---
+++ constants.js
@@ -0,0 +1,17 @@
+const categories = [
+    {
+        image: 'friends',
+        players: '4-6',
+        title: 'Friends',
+    },
+    {
+        image: 'gossip',
+        players: '4-6',
+        title: 'Gossip Girls',
+    },
+    {
+        image: 'suits',
+        players: '2-4',
+        title: 'Suits',
+    },
+]
