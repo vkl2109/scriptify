d11e8b80d38effa7d5f51ba1a56a635ee9718645
Vincent Le <vkl2109@columbia.edu>
2023-12-20 20:38:12 -0800

    Added components folder

---
+++ Components/Divider.js
@@ -0,0 +1,13 @@
+import {
+    StyleSheet,
+    View,
+    Text
+} from 'react-native'
+
+export function Divider ({ text }) {
+    return(
+        <>
+        
+        </>
+    )
+}
--- Screens/MainScreen.js
+++ Screens/MainScreen.js
@@ -1,18 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+    StyleSheet, 
+    View,
+    Text, 
+    TouchableOpacity,
+} from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { Divider } from '../Components'
 
 export function MainScreen () {
+
+    const handleStart = () => {
+
+    }
+
     return(
-        <View style={styles.container}>
-            <Text>Open up App.js to start working on your app!</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <Text style={styles.title}>Scriptify</Text>
+            <View style={styles.lowerHalf}>
+                <TouchableOpacity 
+                    style={styles.buttonStart}
+                    onPress={handleStart}
+                    >
+                        <Text style={styles.startText}>Start Game</Text>
+                </TouchableOpacity>
+                <Divider />
+            </View>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#31304D',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-evenly',
   },
+  lowerHalf: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 50,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  buttonStart: {
+    backgroundColor: '#F0ECE5',
+    width: '90%',
+    height: 50,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 100,
+  },
+  startText: {
+    fontWeight: 'bold',
+    color: '#161A30',
+  }
 });
