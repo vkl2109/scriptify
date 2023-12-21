61471aaa7359bb8808472b7bb73b6be864c1fe04
Vincent Le <vkl2109@columbia.edu>
2023-12-20 19:50:09 -0800

    added safe area provider

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from '@react-navigation/native';
+import { SafeAreaProvider } from 'react-native-safe-area-context'
 import { RootNavigator } from './Navigation'
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <RootNavigator />
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <RootNavigator />
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 }
