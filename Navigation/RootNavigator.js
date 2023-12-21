956f612e3e5db890c65b18c3bdf8e29e4ba4f4f0
Vincent Le <vkl2109@columbia.edu>
2023-12-20 19:41:33 -0800

    added react native navigation, root navigator and main screen

--- Navigation/RootNavigator.js
+++ Navigation/RootNavigator.js
@@ -1,6 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { MainScreen } from '../Screens'
 
+const Stack = createNativeStackNavigator();
 
 export function RootNavigator () {
-    return(<>
-    </>)
+    return(
+        <Stack.Navigator 
+            screenOptions={{ headerShown: false }}
+            initialRouteName={'Main'}
+            >
+            <Stack.Screen name="Main" component={MainScreen} />
+        </Stack.Navigator>
+    )
 }
