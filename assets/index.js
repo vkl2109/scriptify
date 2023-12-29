b0c8467391064164527a945bac4a172800a18997
Vincent Le <vkl2109@columbia.edu>
2023-12-28 17:44:59 -0800

    added new images for main screen

--- assets/index.js
+++ assets/index.js
@@ -1,11 +1,20 @@
 import friends from './friends.jpg'
 import gossip from './gossip.jpeg'
 import suits from './suits.jpg'
+import theater from './theatermasks.png'
+import suspect from './suspect.png'
+import fun from './fun.png'
 
 const images = {
     friends,
     gossip,
     suits,
+}
+
+const introImages = {
+    theater,
+    suspect,
+    fun,
 }
 
 export default images
