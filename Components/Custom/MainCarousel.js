6a802be42650f8ecf70de2d82684bcdef7b6099d
Vincent Le <vkl2109@columbia.edu>
2024-01-30 16:07:46 -0500

    removed flex on carousel to flix landing page glitch

--- Components/Custom/MainCarousel.js
+++ Components/Custom/MainCarousel.js
@@ -96,7 +96,6 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
         flexDirection: "column",
-        flex: 1,
         width: "100%",
     },
     item:(w) => ({
